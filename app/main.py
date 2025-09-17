251c0f97c89269949e90dc3cc74c24641cfede9b
Nazar <your@mail.com>
2025-09-17 18:41:01 +0300

    Solution_3

--- app/main.py
+++ app/main.py
@@ -2,24 +2,23 @@ from __future__ import annotations
 
 
 class Animal:
-    alive = []
+    alive: list["Animal"] = []
 
     def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.health = health
         self.hidden = False
         Animal.alive.append(self)
+        Animal.check_alive(self)
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name}, Health: {self.health}, Hidden: "
                 f"{self.hidden}}}")
 
     @staticmethod
-    def check_alive(herbivore: Herbivore) -> None:
-        if herbivore.health <= 0:
-            for element in Animal.alive:
-                if element.name == herbivore.name:
-                    Animal.alive.remove(element)
+    def check_alive(animal_obj: Animal) -> None:
+        if animal_obj.health <= 0:
+            Animal.alive.remove(animal_obj)
 
 
 class Herbivore(Animal):
@@ -34,5 +33,6 @@ class Carnivore(Animal):
     def bite(self, herbivore: Herbivore) -> None:
         if isinstance(herbivore, Herbivore):
             if not herbivore.hidden:
-                herbivore.health -= 50
+                if herbivore.health > 0:
+                    herbivore.health -= 50
                 Animal.check_alive(herbivore)
